161f8e69df08144973cdc18ec8bccb20ec712076
jhoandavidpll <pjhoandavid@outlook.com>
2025-12-24 00:37:58 -0500

    feat: Agregado nuevas caracteristicas para leer csv y gráficos en streamlit para test.py

--- Frontend/test.py
+++ Frontend/test.py
@@ -12,6 +12,17 @@ st.write("## Este un título H2")
 
 # importar datos
 import pandas as pd
+import numpy as np
 
-data = pd.read_csv()
+data = pd.read_csv("/home/jhoan/Documentos/portfolio/Hackathon-Alura-ONE-SentimentalAPI-E47/Data-Science/data/spanish/train.csv")
+
+st.write(data)
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns = ["a", "b", "c"]
+)
+
+st.bar_chart(chart_data)
+st.line_chart(chart_data)
 
